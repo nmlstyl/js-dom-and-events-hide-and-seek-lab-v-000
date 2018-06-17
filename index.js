3ec6d3fd0791ccd1dce9a79952f492a6489cb94a
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-17 01:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function deepestChild(){
   // return  document.querySelectorAll('#grand-node div div div div')[0]
   let node = document.getElementById('grand-node').querySelectorAll('div')
 
-  for (let i=0; i < node.length; i++)
+  for (let i=0; i < node.length; i++){
+    
+  }
 
 }

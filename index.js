f63e5985d389128814db6556eef3722461dff957
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-17 01:27:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ function nestedTarget(){
 
 function deepestChild(){
   // return  document.querySelectorAll('#grand-node div div div div')[0]
-  let node = document.getElementById('grand-node')
+  let node = document.getElementById('grand-node').querySelectorAll('div')
+
+  
 }

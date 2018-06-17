5a0dca6d3702c9dfb2812c4a69d587dd3c5be7e1
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-06-17 01:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 
+<<<<<<< HEAD
 function nestedTarget(){
   return document.querySelector('.target')
 }
@@ -10,4 +11,8 @@ function deepestChild(){
   for (const div in document.querySelector('grand-node')){
     debugger;
   }
+=======
+function nestedTarget(nested){
+  return nested.target
+>>>>>>> 37824b98ba76f7071f17b07bce7b43a38c238ae0
 }
